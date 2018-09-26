548647c2e10ae4304f8a6f56194aa0dae44e080d
Alessandro Orlandi <aorlandi@buy4now.ie>
2018-09-26 17:02:58 +0100

    [Enhancement] - Store working fine

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -3,7 +3,7 @@ import './Hello.css';
 
 export interface Props {
   name: string;
-  enthusiasmLevel ? : number;
+  enthusiasmLevel : number;
   onIncrement ? : () => void;
   onDecrement ? : () => void;
 }
@@ -14,17 +14,15 @@ class Hello extends React.Component<Props> {
   }
 
   render() {
-    const { name, enthusiasmLevel = 2 } = this.props;
-
-    if (enthusiasmLevel <= 0) {
-      throw new Error('You could be a little more enthusiastic. :D');
-    }
-
     return (
       <div className="hello">
         <div className="greeting">
-          Hello {name + getExclamationMarks(enthusiasmLevel)}
+          Hello {this.props.name + getExclamationMarks(this.props.enthusiasmLevel)}
         </div>
+         <div>
+        <button onClick={this.props.onDecrement}>-</button>
+        <button onClick={this.props.onIncrement}>+</button>
+      </div> 
       </div>
     );
   }
